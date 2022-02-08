38d944348a1fc259855aa3b48bd13b5fe4ffe468
Justin Barnikow <justinsbarnikow@gmail.com>
2022-02-08 11:13:00 -0500

    edit store content

--- src/store.js
+++ src/store.js
@@ -2,28 +2,33 @@ const store = {
     lists: [
       {
         id: '1',
-        header: 'To Do (home)',
+        header: 'To-do',
         cardIds: [ 'a', 'b'],
       },
       {
         id: '2',
-        header: 'To Do (work)',
+        header: 'Cleaning',
         cardIds: ['b', 'c', 'd', 'f', 'h'],
       },
       {
         id: '3',
         header: 'Groceries',
-        cardIds: [ 'a', 'b', 'd', 'f', 'g', 'h', 'i', 'j', 'k' ],
+        cardIds: ['b', 'd', 'f', 'g', 'h', 'i', 'j', 'k' ],
       },
       {
         id: '4',
-        header: 'Chores',
+        header: 'Appointments',
         cardIds: ['e', 'l', 'm' ],
       },
+      {
+        id: '5',
+        header: 'New List',
+        cardIds: ['d']
+      }
     ],
     allCards: {
-      'a': { id: 'a', title: 'First card', content: 'lorem ipsum' },
-      'b': { id: 'b', title: 'Second card', content: 'lorem ipsum' },
+      'a': { id: 'a', title: 'Make a list app', content: 'Edit the titles and content with a double-click!' },
+      'b': { id: 'b', title: 'Repetitive Card', content: 'This card is in more than one list. What happens when you delete me?' },
       'c': { id: 'c', title: 'Third card', content: 'lorem ipsum' },
       'd': { id: 'd', title: 'Fourth card', content: 'lorem ipsum' },
       'e': { id: 'e', title: 'Fifth card', content: 'lorem ipsum' },
